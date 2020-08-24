a106d67718dda0c26a14e41fa2a99fb0895e4049
miwebst <50640090+miwebst@users.noreply.github.com>
2020-08-24 13:51:46 -0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,4 @@
 #!/bin/sh -l
 cd /bin/staticsites/
-./StaticSitesClient $INPUT_ACTION --oryxEnabled true
+# ./StaticSitesClient $INPUT_ACTION --oryxEnabled true
+echo "::set-output name=static_web_app_deploy_url::https://nice-meadow-0c9756810.azurestaticapps.net"
